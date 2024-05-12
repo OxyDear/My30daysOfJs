0f2236758ad21aab6c4b2792f6da8941a56c66a6
freestyle.06@mail.ru <freestyle.06@mail.ru>
2024-05-13 01:38:22 +0300

    23 day my js

--- 23_Day_Event_listeners/23_day_starters/project_1/scripts/main.js
+++ 23_Day_Event_listeners/23_day_starters/project_1/scripts/main.js
@@ -1,2 +1,48 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+const body = document.querySelector('body')
+const wrapper = document.querySelector('div');
+const ipt = document.querySelector('#amount');
+const btn = document.querySelector('#amountButton');
+
+body.style.textAlign = 'center';
+body.style.fontFamily = 'Oswald';
+wrapper.style.display = 'flex';
+wrapper.style.flexWrap = 'wrap';
+wrapper.style.justifyContent = 'center';
+
+let amount = 0;
+btn.addEventListener('click', () => displayNumbers(ipt.value))
+
+function displayNumbers (amount) {
+    wrapper.innerHTML = ''
+    let number;
+    let color;
+    for (let num = 0; num <= amount; num++) {
+        if (num % 6 === 0) {
+            number = document.createElement('p');
+            wrapper.appendChild(number);
+        }
+        if (num === 1) {
+            color = '#ECCE72';
+        } else if (num === 2) {
+            color = 'red';
+        }
+        if (num % 2 === 0 && num !== 2) {
+            color = 'green';
+        } else {
+            for (let n = 2; n < num; n++) {
+                if (num % n === 0) {
+                    color = '#ECCE72';
+                    break;
+                } else {
+                    color = 'red';
+                }
+            }
+        }
+        number.innerHTML = number.innerHTML + ` <span id="${num}" style="margin: 5px; background-color: ${color}; width: 50px; height: 25px; font-weight: bold; color: beige">${num}</span>`;
+        number.style.display = 'flex';
+        number.style.marginBottom = '-20px';
+        number.style.justifyContent = 'center';
+        number.style.textAlign = 'center';
+    }
+}
--- 23_Day_Event_listeners/23_day_starters/project_2/scripts/main.js
+++ 23_Day_Event_listeners/23_day_starters/project_2/scripts/main.js
@@ -1,2 +1,10 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+let body = document.querySelector('body')
+let wrapper = document.querySelector('#wrapper')
+let box = document.querySelector('.box')
+wrapper.innerHTML = 'Press any keyboard key'
+
+document.addEventListener('keypress', (e) => {
+    wrapper.innerHTML = `<span>You pressed <span style="font-weight: bold; color: #12a112">${e.key}</span></span>`
+    box.innerHTML = `<div id="box">${e.keyCode}</div>`
+})
